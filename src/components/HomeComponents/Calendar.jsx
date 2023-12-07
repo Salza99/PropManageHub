d89e6c7656d702417607dd870a97455b1e1877d1
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-12-07 03:34:24 +0100

    Update Calendar.jsx

--- src/components/HomeComponents/Calendar.jsx
+++ src/components/HomeComponents/Calendar.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
 import dayjs from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
 import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 
 const Calendar = () => {
