0d45815298daf984057e688505448bad30494d5e
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-12-13 16:12:40 +0100

    loader Page scss done

--- src/components/Loaders/PageLoader.jsx
+++ src/components/Loaders/PageLoader.jsx
@@ -1,7 +1,11 @@
 const PageLoader = () => {
   return (
-    <div className="h-100 d-flex align-items-center justify-content-center">
-      <p className="fs-1 fw-bold">PropManageHub</p>
+    <div style={{ height: "100vh" }} className="d-flex align-items-center justify-content-center">
+      <div className="position-relative">
+        <p className="fw-bold text-white text-loader">PropManageHub</p>
+        <div className="loader-page"></div>
+        <div className="transparent-circle"></div>
+      </div>
     </div>
   );
 };
