ca46049cbc06595795faccbf61a13f0de8686e71
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-12-22 00:55:01 +0100

    Persist store implement

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit";
 import LoginReducer from "../reducers/LoginReducer";
 import HomepageReducer from "../reducers/HomepageReducer";
 import CustomerReducer from "../reducers/CustomerReducer";
@@ -6,7 +6,9 @@ import PropertyReducer from "../reducers/PropertyReducer";
 import RequestReducer from "../reducers/RequestReducer";
 import AdminReducer from "../reducers/AdminReducer";
 import AddressReducer from "../reducers/AddressReducer";
-
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
 const rootReducer = combineReducers({
   login: LoginReducer,
   home: HomepageReducer,
@@ -17,7 +19,10 @@ const rootReducer = combineReducers({
   address: AddressReducer,
 });
 
+const persistedReducer = persistReducer({ key: "root", storage }, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
+export const persistor = persistStore(store);
 export default store;
